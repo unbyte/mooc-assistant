b0f329d2814c1e17de000afa85c5726735c2fe6a
Helios <i@shangyes.net>
2020-05-26 16:28:45 +0800

    chore: previous fix works
    66b9972a30bd2c2b9b71e1dad34d86baf94c7788
    
    close #6, #8

--- src/utils/hack.ts
+++ src/utils/hack.ts
@@ -1,3 +1,4 @@
+// to anti `EventTarget.prototype.addEventListener = function(){check whether a click is from real user}`
 export const preventBanClick: () => void = () => {
   const saveScript = document.createElement("script");
   saveScript.textContent = `Object.freeze(EventTarget.prototype);`;
